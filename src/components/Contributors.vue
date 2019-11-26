b175fca828d82c2462da857055094a881f2fe589
Mohit Gupta <mohitgupta1918@gmail.com>
2019-11-26 12:10:31 +0530

    Add page contributors

---
+++ src/components/Contributors.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Contributors</h1>
+        <ul>
+            <li>Binny VA</li>
+            <li>Rohit Nair</li>
+            <li>Mohit Gupta</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contributors",
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped lang="scss"></style>
