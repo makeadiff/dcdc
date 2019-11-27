ebde9a63a01a467bcbb023652089ae43f64c4684
rhit99900 <rohitcse081@gmail.com>
2019-11-27 12:23:11 +0530

    Update Styling to msuite-vue

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
 
 import "../node_modules/nprogress/nprogress.css";
+import "../node_modules/msuite-vue/dist/msuite-vue.css";
+import "msuite-vue";
 
 Vue.config.productionTip = false;
 
@@ -12,3 +16,10 @@ new Vue({
     store,
     render: h => h(App)
 }).$mount("#app");
+
+Sentry.init({
+    dsn: "https://da913f1dd2784dd9b79728ea9dad30bd@sentry.io/1823227",
+    integrations: [
+        new Integrations.Vue({ Vue, attachProps: true, logErrors: true })
+    ]
+});
